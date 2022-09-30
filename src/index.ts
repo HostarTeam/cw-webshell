0e903c436322233fff06834733a89d8b85ed320b
Idan Carlebach <idanefrat@gmail.com>
2022-09-30 22:08:30 +0000

    Successfully migrated to commander

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,30 @@
-import { isValidIP, printFatal } from './lib/utils';
+import { parsePort, validateHost } from './lib/utils';
 import WebShell from './WebShell';
-
-function handleArgsError(message: string): void | never {
-	printFatal(message);
-	console.log('Usage: ./cw-webshell <token> <port> <host>');
-	process.exit(1);
-}
+import { program } from 'commander';
 
 function main() {
-	const args = process.argv.slice(2);
-	if (args.length !== 3)
-		handleArgsError(
-			'Invalid number of arguments. Expected 3, got ' + args.length
-		);
+	program
+		.name('ws-webshell')
+		.description('Container Workspaces WebShell service')
+		.version('1.0.0');
 
-	// eslint-disable-next-line prefer-const
-	let [token, port, host] = args;
-	if (!Number.isInteger(Number(port)))
-		handleArgsError('Invalid port. Expected integer, got ' + port);
-
-	if (parseInt(port) < 0 || parseInt(port) > 65535)
-		handleArgsError('Invalid port. Expected 0-65535, got ' + port);
-
-	if (host === 'localhost') host = '127.0.0.1';
-
-	if (!isValidIP(host))
-		handleArgsError('Invalid host. Expected valid IP, got ' + host);
-
-	new WebShell({
-		token,
-		port: parseInt(port),
-		host,
-	});
+	program
+		.argument('<token>', 'The token to use for authentication')
+		.option('-p, --port <number>', 'The port to listen on', parsePort, 8444)
+		.option(
+			'-h, --host <string>',
+			'The host to connect to',
+			validateHost,
+			'0.0.0.0'
+		)
+		.action((token, opts: { port: number; host: string }) => {
+			new WebShell({
+				token,
+				port: opts.port,
+				host: opts.host,
+			});
+		})
+		.parse();
 }
 
 if (require.main === module) main();
